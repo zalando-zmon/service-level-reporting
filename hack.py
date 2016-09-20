783587e57a9344938484aeec46e8a5083565abd1
Henning Jacobs <henning@jacobs1.de>
2016-09-20 17:06:55 +0200

    allow weighted average and sum aggregation

--- hack.py
+++ hack.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 import click
+import psycopg2
 import requests
 import zign.api
 
 
 @click.command()
 @click.option('--kairosdb-url')
-@click.option('--metric-name')
+@click.option('--check-id', type=int)
+@click.option('--sli-name')
+@click.option('--start', type=int, default=5)
+@click.option('--time-unit', default='minutes')
 @click.option('--key', '-k', multiple=True)
-def cli(kairosdb_url, metric_name, key):
-    # do stuff
+@click.option('--exclude-key', '-x', multiple=True)
+@click.option('--aggregation')
+@click.option('--dsn')
+@click.option('--weight-pattern', default='rate')
+def cli(kairosdb_url, check_id, sli_name, start, time_unit, key, exclude_key, aggregation, dsn, weight_pattern):
+    kairosdb_metric_name = 'zmon.check.{}'.format(check_id)
 
     token = zign.api.get_token('zmon', ['uid'])
     headers = {'Authorization': 'Bearer {}'.format(token)}
@@ -20,11 +28,11 @@ def cli(kairosdb_url, metric_name, key):
 
     q = {
             'start_relative': {
-                'value': 5,
-                'unit': 'minutes'
+                'value': start,
+                'unit': time_unit
             },
             'metrics': [{
-                'name': metric_name,
+                'name': kairosdb_metric_name,
                 'tags': {'key': key},
                 'group_by': [{'name': 'tag', 'tags': ['entity', 'key']}]
             }]
@@ -34,8 +42,54 @@ def cli(kairosdb_url, metric_name, key):
     response.raise_for_status()
 
     data = response.json()
+
+    res = {}
     for result in data['queries'][0]['results']:
-        print(result['group_by'])
+        group = result['group_by'][0]['group']
+        key = group['key']
+        exclude = False
+        for excl in exclude_key:
+            if excl in key:
+                exclude = True
+        if not exclude:
+            for ts, value in result['values']:
+                # truncate to full minutes
+                minute = (ts // 60000) * 60
+                if minute not in res:
+                    res[minute] = {}
+                g = group['entity'], '.'.join(key.split('.')[:-1])
+                if g not in res[minute]:
+                    res[minute][g] = {}
+                if aggregation == 'weighted' and weight_pattern in key.lower():
+                    res[minute][g]['weight'] = value
+                else:
+                    res[minute][g]['value'] = value
+
+    print(res)
+    res2 = {}
+    for minute, values in res.items():
+        if aggregation == 'weighted':
+            total_weight = 0
+            total_value = 0
+            for g, entry in values.items():
+                if 'weight' in entry and 'value' in entry:
+                    total_weight += entry['weight']
+                    total_value += entry['value'] * entry['weight']
+            res2[minute] = total_value / total_weight
+        elif aggregation == 'sum':
+            total_value = 0
+            for g, entry in values.items():
+                total_value += entry['value']
+            res2[minute] = total_value
+
+    print(res2)
+
+    if dsn:
+        conn = psycopg2.connect(dsn)
+        cur = conn.cursor()
+        for minute, val in res2.items():
+            cur.execute('INSERT INTO foo (sli_name, sli_value) VALUES (%s, %s)', (sli_name, val))
+        conn.commit()
 
 
 
