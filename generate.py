84b1604b5c9491a96b2852495c44f83f275d252b
Henning Jacobs <henning@jacobs1.de>
2016-09-28 12:30:47 +0200

    sort SLIs

--- generate.py
+++ generate.py
@@ -3,11 +3,16 @@
 import collections
 import jinja2
 import os
+import re
 import requests
 import sys
 import zign.api
 
 import plot
+
+
+def title(s):
+    return s.title().replace('_', ' ').replace('.', ' ')
 
 base_url = sys.argv[1]
 product = sys.argv[2]
@@ -26,8 +31,6 @@ values_by_sli = collections.defaultdict(list)
 for day, data in sorted(report_data.items()):
     for sli_name, _sli_data in data.items():
         values_by_sli[sli_name].append(_sli_data['avg'])
-    print(day)
-    print(data)
 
 loader = jinja2.FileSystemLoader('templates')
 env = jinja2.Environment(loader=loader)
@@ -40,25 +43,36 @@ data = {
 for slo in slos:
     slo['slis'] = {}
     for target in slo['targets']:
-        slo['slis'][target['sli_name']] = '{:.2f}{}'.format(sum(values_by_sli[target['sli_name']]) / len(values_by_sli[target['sli_name']]), target['unit'])
+        slo['slis'][target['sli_name']] = '{:.2f} {}'.format(sum(values_by_sli[target['sli_name']]) / len(values_by_sli[target['sli_name']]), target['unit'])
     slo['data'] = []
     breaches_by_sli = collections.defaultdict(int)
     for day, day_data in sorted(report_data.items()):
         slis = {}
+        unit = ''
         for sli, sli_data in day_data.items():
             breaches_by_sli[sli] += sli_data['breaches']
             flag = ''
             if sli_data['breaches']:
                 flag = 'orange'
-            slis[sli] = {'value': sli_data['avg'], 'flag': flag}
+            for target in slo['targets']:
+                if target['sli_name'] == sli:
+                    unit = target['unit']
+                    if target['to'] and sli_data['avg'] > target['to']:
+                        flag = 'red'
+                    elif target['from'] and sli_data['avg'] < target['from']:
+                        flag = 'red'
+
+            slis[sli] = {'value': sli_data['avg'], 'flag': flag, 'unit': unit}
         slo['data'].append({'caption': day[5:10], 'slis': slis})
     slo['breaches'] = max(breaches_by_sli.values())
 
-    fn = 'output/{}.png'.format(slo['id'])
+    fn = 'output/chart-{}-{}-{}-{}.png'.format(product, slo['id'], min(report_data.keys()), max(report_data.keys()))
+    fn = re.sub('[^/0-9a-zA-Z-]', '', fn)
     plot.plot(base_url, product, slo['id'], fn)
     slo['chart'] = os.path.basename(fn)
     data['slos'].append(slo)
 
+env.filters['sli_title'] = title
 template = env.get_template('slr.tpl')
 os.makedirs('output', exist_ok=True)
 template.stream(**data).dump('output/slr.html')
--- templates/slr.tpl
+++ templates/slr.tpl
@@ -2,7 +2,8 @@
 <head>
     <title>Service Level Report - {{period}}</title>
     <style>
-    body { font: 13px Arial, Helvetica, sans-serif; text-align: center; }
+    body { font: 16px Arial, Helvetica, sans-serif; text-align: center; }
+    th, td { font-size: 16px; }
     td.value { text-align: center; }
     .sli-large { font-size: 48px; text-align: center; }
     .sli-caption { text-align: center; }
@@ -19,13 +20,17 @@
 <h2>Reliability Index</h2>
 <table style="width:100%">
     <tr>
-        {% for sli in slo.slis.values() %}
-        <th class="sli-large">{{sli}}</th>
+        {% for sli in slo.slis.keys()|sort %}
+        {% if sli != 'requests': %}
+        <th class="sli-large">{{slo.slis[sli]}}</th>
+        {% endif %}
         {% endfor %}
     </tr>
     <tr>
-        {% for sli in slo.slis.keys() %}
-        <td class="sli-caption">{{sli}}</td>
+        {% for sli in slo.slis.keys()|sort %}
+        {% if sli != 'requests': %}
+        <td class="sli-caption">{{sli|sli_title}}</td>
+        {% endif %}
         {% endfor %}
     </tr>
 </table>
@@ -34,16 +39,18 @@
     <tr>
         <th>SLI</th>
         {% for sli in slo.data %}
-        <th>{{sli.caption}}</th>
+        <th>{{sli.caption|sli_title}}</th>
         {% endfor %}
     </tr>
 
-    {% for sli in slo.slis.keys() %}
+    {% for sli in slo.slis.keys()|sort %}
     <tr>
-        <th>{{sli}}</th>
+        <th>{{sli|sli_title}}</th>
         {% for data in slo.data %}
         {% if data.slis.get(sli) %}
-        <td class="value" {{ 'bgcolor="' + data.slis.get(sli).get('flag') + '"' if data.slis.get(sli).get('flag') }}>{{'%.2f'|format(data.slis.get(sli).get('value'))}}</td>
+        <td class="value" {{ 'bgcolor="' + data.slis.get(sli).get('flag') + '"' if data.slis.get(sli).get('flag') }}>{{'%.2f'|format(data.slis.get(sli).get('value'))}} {{data.slis.get(sli).get('unit') }}</td>
+        {% else %}
+        <td></td>
         {% endif %}
         {% endfor %}
     </tr>
