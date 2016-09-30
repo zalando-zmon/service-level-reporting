319f3ca345491df8d8437ee726334986621a109c
Henning Jacobs <henning@jacobs1.de>
2016-09-30 08:23:46 +0200

    return SLIs from day start of 7 days ago

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_service_level_indicators(product, name, time_from=None, time_to=None):
         cur = conn.cursor()
         cur.execute('''SELECT sli_timestamp, sli_value from zsm_data.service_level_indicator
         WHERE sli_product_id = (SELECT p_id FROM zsm_data.product WHERE p_slug = %s) AND sli_name = %s
-        AND sli_timestamp >= \'now\'::timestamp - interval \'7 days\'
+        AND sli_timestamp >= date_trunc(\'day\', \'now\'::timestamp - interval \'7 days\')
         ORDER BY 1''', (product, name))
         return cur.fetchall()
 
