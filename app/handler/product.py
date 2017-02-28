6ed098ef4e344efe279a6ee3dca00cf7965420ae
Arjun Naik <arjun@arjunnaik.in>
2017-02-28 22:53:51 +0100

    Added filtering for products based on product group.

--- app/handler/product.py
+++ app/handler/product.py
@@ -10,12 +10,18 @@ from app.utils import strip_column_prefix, slugger
 logger = logging.getLogger('slo-product')
 
 
-def get():
+def get(pg):
     with dbconn() as conn:
         cur = conn.cursor()
+        param_dict = {}
+        if pg:
+            param_dict['pg'] = pg
+            clause = ' AND pg_slug = %(pg)s '
+        else:
+            clause = ''
         cur.execute('''SELECT p.*, pg_name AS pg_product_group_name, pg_slug AS pg_product_group_slug, pg_department
-                FROM zsm_data.product p
-                JOIN zsm_data.product_group ON pg_id = p_product_group_id''')
+                FROM zsm_data.product p, zsm_data.product_group pg WHERE pg_id = p_product_group_id''' + clause,
+                    param_dict)
         rows = cur.fetchall()
         res = [strip_column_prefix(r._asdict()) for r in rows]
     return res
