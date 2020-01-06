62587fb338a62d111aa1b9f1afb2c677d824e66f
Marcin Zaremba <marcin.zaremba@zalando.de>
2020-01-06 12:27:16 +0100

    Python and formatting

--- common/lightstep.py
+++ common/lightstep.py
@@ -1,7 +1,9 @@
-import requests
 import os
-import dateutil.parser
 from datetime import datetime
+
+import dateutil.parser
+import requests
+
 from datetime_truncate import truncate
 
 
@@ -21,11 +23,14 @@ def get_individual_metric_from_stream(stream_data, position, metric):
 
 def extract_metric_from_stream(stream_data, metric):
     result = []
-    data_points = stream_data['data']['attributes']['points-count']
-    for i in range(0, data_points):
+    data_points = stream_data["data"]["attributes"]["points-count"]
+    for i in range(data_points):
         timestamp = truncate(
-            dateutil.parser.parse(stream_data["data"]["attributes"]["time-windows"][i]["youngest-time"]),
-            'second').replace(tzinfo=None)
+            dateutil.parser.parse(
+                stream_data["data"]["attributes"]["time-windows"][i]["youngest-time"]
+            ),
+            "second",
+        ).replace(tzinfo=None)
         value = get_individual_metric_from_stream(stream_data, i, metric)
         result.append(StreamDataPoint(timestamp=timestamp, value=value))
     return result
@@ -33,56 +38,46 @@ def extract_metric_from_stream(stream_data, metric):
 
 def get_api_params_for_metric(metric):
     if metric == "ops-count":
-        return {
-            "include-ops-counts": "1"
-        }
+        return {"include-ops-counts": "1"}
     elif metric == "error-count":
-        return {
-            "include-error-counts": "1"
-        }
+        return {"include-error-counts": "1"}
     elif metric == "p50":
-        return {
-            "percentile": "50"
-        }
+        return {"percentile": "50"}
     elif metric == "p75":
-        return {
-            "percentile": "75"
-        }
+        return {"percentile": "75"}
     elif metric == "p90":
-        return {
-            "percentile": "90"
-        }
+        return {"percentile": "90"}
     elif metric == "p99":
-        return {
-            "percentile": "99"
-        }
+        return {"percentile": "99"}
 
 
-def get_stream_data(stream_id, timestamp_start: datetime, timestamp_end: datetime, metric):
+def get_stream_data(
+    stream_id, timestamp_start: datetime, timestamp_end: datetime, metric
+):
     timestamp_start_iso = timestamp_start.replace(microsecond=0).isoformat() + "Z"
     timestamp_end_iso = timestamp_end.replace(microsecond=0).isoformat() + "Z"
 
-    params = {**{
-        "oldest-time": timestamp_start_iso,
-        "youngest-time": timestamp_end_iso,
-        "resolution-ms": "600000",
-        # Lightstep has an internal server error at a resolution of 60000, for more than 2 days
-        "include-ops-counts": "1" if metric == "ops-count" else "0",
-        "percentile": "90",
-        "include-error-counts": "1"
-    }, **get_api_params_for_metric(metric)}
-
-    response = requests.request(
-        "GET",
-        url=f"https://api.lightstep.com/public/v0.1/Zalando/projects/Production/searches/{stream_id}/timeseries",
-        headers={
-            "Authorization": f'Bearer {os.getenv("LIGHTSTEP_API_KEY")}'
+    params = {
+        **{
+            "oldest-time": timestamp_start_iso,
+            "youngest-time": timestamp_end_iso,
+            "resolution-ms": "600000",
+            # Lightstep has an internal server error at a resolution of 60000, for more than 2 days
+            "include-ops-counts": "1" if metric == "ops-count" else "0",
+            "percentile": "90",
+            "include-error-counts": "1",
         },
-        params=params
+        **get_api_params_for_metric(metric),
+    }
+
+    response = requests.get(
+        url=f"https://api.lightstep.com/public/v0.1/Zalando/projects/Production/searches/{stream_id}/timeseries",
+        headers={"Authorization": f'Bearer {os.getenv("LIGHTSTEP_API_KEY")}'},
+        params=params,
     )
     response_json = response.json()
-    errors = response_json.get('errors', None)
+    errors = response_json.get("errors")
     if errors:
-        raise Exception('Lightstep returned errors', errors)
+        raise Exception("Lightstep returned errors", errors)
 
     return extract_metric_from_stream(response_json, metric)
